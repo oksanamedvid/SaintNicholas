3a5381919260d6d1c71132c06588d877cfe37220
Sana1007 <oksana.medvid14@gmail.com>
2019-05-18 12:48:29 +0300

    Add project files.

---
+++ SaintNicholas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SaintNicholas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
